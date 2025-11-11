7d024149fcf3847c176cec29d55f57712a9c7868
Zachary McGuire <zacharysjmcguire@gmail.com>
2025-11-11 13:50:43 +1100

    1.2 solution.

--- ch1/Exercise 1.2/main.go
+++ ch1/Exercise 1.2/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
+	"strconv"
 )
 
 func main() {
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	for i, arg := range os.Args[:] {
+		if i == 0 {
+			continue // Don't print arg 0, the name of the command.
+		}
+		fmt.Println("Argument " + strconv.Itoa(i) + ": " + arg) // Alternatively, i + 1 here.
+	}
 }
