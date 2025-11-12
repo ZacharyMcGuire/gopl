fe4b0c4eb7097b87465c2f2a6deee47d14af210b
Zachary McGuire <zacharymcguire@pm.me>
2025-11-12 16:00:59 +1100

    1.5 solution.

--- ch1/1.5/main.go
+++ ch1/1.5/main.go
@@ -10,11 +10,11 @@ import (
 	"os"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{color.Black, color.RGBA{0x00, 0x99, 0x00, 0x99}}
 
 const (
-	whiteIndex = 0
-	blackIndex = 1
+	backgroundIndex = 0
+	lineColorIndex  = 1
 )
 
 func main() {
@@ -39,7 +39,7 @@ func lissajous(out io.Writer) {
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), lineColorIndex)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
